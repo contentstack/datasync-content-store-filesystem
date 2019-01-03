f6026cb1dca0130ac79948f2799e2bcdab0c09cc
asmit-patil <patilasmit0@gmail.com>
2019-01-03 19:06:00 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+// import { existsSync } from 'fs'
+// import { join } from 'path'
+// import { get } from '../config'
+// import { render } from '../util'
+// import { messages as msg } from '../util/messages'
+import Filesystem from './core/filesystem'
+import { merge } from "lodash";
+
+let connector = null
+let config: any = {}
+export function start (userConfig, assetConnector) {
+  try {
+    return new Promise((resolve, reject) => {
+      if (userConfig) {
+        //console.log("userconfig");
+        config = merge(config, userConfig)
+      } else {
+        console.log("Starting connector with default configs");
+      }
+      //build(config)
+      connector = new Filesystem(config, assetConnector)
+      resolve(connector)
+    })
+  } catch (error) {
+    console.error(error)
+
+  }
+}
+
+export function getConnectorInstance () {
+	return connector
+}
