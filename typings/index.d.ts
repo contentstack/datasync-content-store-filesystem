7944a647b078ee627e2ea27a2eb14963604a7b62
asmit-patil <patilasmit0gmail.com>
2019-02-25 18:35:16 +0530

    updated .gitignore and test files

---
+++ typings/index.d.ts
@@ -0,0 +1,16 @@
+/*!
+* contentstack-sync-content-store-filesystem
+* copyright (c) Contentstack LLC
+* MIT Licensed
+*/
+/**
+ * @description to start the content connector
+ * @param  {} assetConnector: asset connector instance
+ * @param  {} config?: config
+ * @param  {} logger?: logger instance
+ */
+export declare function start(assetConnector: any, config?: any): Promise<{}>;
+/**
+ * @description to get connector instance
+ */
+export declare function getConnectorInstance(): any;
