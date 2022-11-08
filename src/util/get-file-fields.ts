ed22e520c319c1848e0e0ca5344275b753b65955
Vikram Kalta <vikram.kalta@contentstack.com>
2022-11-08 17:22:06 +0000

    feat: added support to nullify asset referenced fields when asset is unpublished

--- src/util/get-file-fields.ts
+++ src/util/get-file-fields.ts
@@ -1,10 +1,14 @@
-export const getFileFields = (schema, fileFields = [], parent = '') => {
+export const getFileFieldPaths = (schema, fileFields = [], parent = '') => {
   for (const field of schema.schema) {
     if (field.data_type === 'file') {
-      fileFields.push(parent + '.' + field.uid);
+      fileFields.push(`${parent ? `${parent}.` : ''}${field.uid}`);
     }
-    if (field.schema && field.schema.length) {
-      getFileFields(field.schema, fileFields, parent + '.' + field.uid);
+    if (field.schema?.length) {
+      getFileFieldPaths(field, fileFields, `${parent ? `${parent}.` : ''}${field.uid}`);
+    } else if (field.blocks?.length) {
+      for (const block of field.blocks)  {
+        getFileFieldPaths(block, fileFields, `${parent ? `${parent}.` : ''}${field.uid}`);
+      }
     }
   }
 
