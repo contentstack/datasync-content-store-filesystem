a6676ca8162fda71142c641b95dbb0e3dd90628f
raj pandey <raj.pandey@contentstack.com>
2025-02-10 15:49:24 +0530

    Fix: Update the regex

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,88 +1,92 @@
-import {
-  existsSync,
-} from 'fs'
-import {
-  sync,
-} from 'mkdirp'
-import {
-  isAbsolute,
-  join,
-  sep,
-} from 'path'
+import { existsSync } from "fs";
+import { sync } from "mkdirp";
+import { isAbsolute, join, sep } from "path";
 
-const filterKeys = ['_content_type', '_checkpoint', '_type']
+const filterKeys = ["_content_type", "_checkpoint", "_type"];
 
 export const filter: any = (data) => {
-  const result = {}
+  const result = {};
   for (const key in data) {
     if (filterKeys.indexOf(key) === -1) {
-      result[key] = data[key]
+      result[key] = data[key];
     }
   }
 
-  return result
-}
+  return result;
+};
 
 export const getPathKeys = (patternKeys, json) => {
-  const pathKeys = []
+  const pathKeys = [];
   for (let i = 0, keyLength = patternKeys.length; i < keyLength; i++) {
-    if (patternKeys[i].charAt(0) === ':') {
-      let k = patternKeys[i].substring(1)
-      const idx = k.indexOf('.json')
+    if (patternKeys[i].charAt(0) === ":") {
+      let k = patternKeys[i].substring(1);
+      const idx = k.indexOf(".json");
       // tslint:disable-next-line: no-bitwise
       if (~idx) {
-        k = k.slice(0, idx)
+        k = k.slice(0, idx);
       }
       if (json[k]) {
-        pathKeys.push(json[k])
+        pathKeys.push(json[k]);
       } else {
-        throw new TypeError(`The key ${k} did not exist on ${JSON.stringify(json)}`)
+        throw new TypeError(
+          `The key ${k} did not exist on ${JSON.stringify(json)}`
+        );
       }
     } else {
-      pathKeys.push(patternKeys[i])
+      pathKeys.push(patternKeys[i]);
     }
   }
 
-  return pathKeys
-}
+  return pathKeys;
+};
 
 export const removeUnwantedKeys = (unwanted, json) => {
   for (const key in unwanted) {
     if (unwanted[key] && json.hasOwnProperty(key)) {
-      delete json[key]
+      delete json[key];
     }
   }
 
-  return json
-}
+  return json;
+};
 
 export const normalizeBaseDir = (config) => {
   if (isAbsolute(config.baseDir)) {
     if (!existsSync(config.baseDir)) {
-      sync(config.baseDir)
+      sync(config.baseDir);
     }
   } else {
-    const projectDir = join(__dirname, '..', '..', '..', '..', '..')
-    const contentDir = join(sanitizePath(projectDir), sanitizePath(config.baseDir))
+    const projectDir = join(__dirname, "..", "..", "..", "..", "..");
+    const contentDir = join(
+      sanitizePath(projectDir),
+      sanitizePath(config.baseDir)
+    );
     if (!existsSync(contentDir)) {
-      sync(contentDir)
+      sync(contentDir);
     }
   }
 
-  return config
-}
+  return config;
+};
 
 export const buildLocalePath = (appConfig) => {
-  const localePath = join(sanitizePath(appConfig.baseDir), sanitizePath(appConfig.internal.locale))
-  const localePathArr = localePath.split(sep)
-  localePathArr.splice(localePathArr.length - 1)
-  const localeFolderPath = join.apply(this, localePathArr)
+  const localePath = join(
+    sanitizePath(appConfig.baseDir),
+    sanitizePath(appConfig.internal.locale)
+  );
+  const localePathArr = localePath.split(sep);
+  localePathArr.splice(localePathArr.length - 1);
+  const localeFolderPath = join.apply(this, localePathArr);
 
   if (!existsSync(localeFolderPath)) {
-    sync(localeFolderPath)
+    sync(localeFolderPath);
   }
 
-  return localePath
-}
+  return localePath;
+};
 
-const sanitizePath = (str: string) => str?.replace(/^(\.\.(\/|\\|$))+/, '');
+const sanitizePath = (str: string) =>
+  str
+    ?.replace(/^([\/\\]){2,}/, "./") // Normalize leading slashes/backslashes to ''
+    .replace(/[\/\\]+/g, "/") // Replace multiple slashes/backslashes with a single '/'
+    .replace(/(\.\.(\/|\\|$))+/g, ""); // Remove directory traversal (../ or ..\)
